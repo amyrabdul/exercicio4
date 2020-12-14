8e2b97d3be86dabcad42f0f5005cbac5d3f207d7
Your Name <you@example.com>
2020-12-13 22:52:29 -0300

    exercicio 4

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Double f,c;
+           
+            Console.WriteLine("Calculadora transformadora de Farenreit para Celsius");
+            
+             Console.Write("Digite a sua temperatura em Farenreit : ");
+            f = double.Parse(Console.ReadLine());
+            
+
+            c = (f-32)/1.8;
+
+         Console.WriteLine($"A sua temperatura em celsius é de {c}");
+            Console.WriteLine();
+
+        }
+    }
+}
